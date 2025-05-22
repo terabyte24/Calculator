0f71aedb8b91c587e26afe3c2031282873e3f4fa
terabyte24 <mark1120241@gmail.com>
2025-05-22 20:15:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//calculator programme
+ const display = document.getElementById("display");
+ function appendtodisplay(input){
+    display.value += input;
+ }
+
+function cleardisplay(){
+    display.value="";
+}
+function calculate(){
+
+   try{
+     display.value =eval(display.value);
+   }
+   catch(error){
+    display.value="error";
+   }
+}
